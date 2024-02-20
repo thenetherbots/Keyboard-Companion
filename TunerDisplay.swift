3a5c0ce7735db3a53a903f742fa82df099bbaafd
PranavBarani12 <160355702+PranavBarani12@users.noreply.github.com>
2024-02-20 11:59:32 -0800

    Update TunerDisplay.swift

--- TunerDisplay.swift
+++ TunerDisplay.swift
@@ -1,1 +1,10 @@
+import SwiftUI
 
+@main // Marks this struct as the entry point of the application.
+struct TunerDisplay: App {
+    var body: some Scene {
+        WindowGroup {
+            TunerView() // Your TunerView is called here.
+        }
+    }
+}
