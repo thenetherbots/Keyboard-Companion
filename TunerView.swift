d4413b9c5cc94a20c7440b7344566b5830a0ede4
PranavBarani12 <160355702+PranavBarani12@users.noreply.github.com>
2024-02-20 11:58:20 -0800

    Update TunerView.swift

--- TunerView.swift
+++ TunerView.swift
@@ -1,1 +1,18 @@
+import SwiftUI
+
+struct TunerView: View {
+    @StateObject private var viewModel = AudioViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Detected Pitch: \(viewModel.detectedPitch)")
+                .font(.title)
+            Text("Frequency: \(viewModel.detectedFrequency)")
+                .font(.subheadline)
+        }
+        .onAppear {
+            viewModel.startAudioProcessing()
+        }
+    }
+}
 
